82f577c06b055af968be326d32aa5b153b296037
dcardonamoreno <danielcardona3d@gmail.com>
2022-07-19 03:54:06 -0500

    render de movemente of the bars and render the ball

--- main.js
+++ main.js
@@ -24,10 +24,30 @@
             return elements;
         }
     }
-
-
 })();
 
+
+/**
+ * set ball parameters
+ */
+
+(function () {
+    self.Ball = function (x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
 
 /**
  * setear las barras
@@ -70,22 +90,33 @@
     }
 
     self.BoardView.prototype = {
+        clean: function(){
+            this.ctx.clearRect(0, 0, board.width, board.height);
+        },
         draw: function () {
             for (let i = this.board.elements.length - 1; i >= 0; i--) {
                 let el = this.board.elements[i];
                 draw(this.ctx, el);
             }
+        },
+        play: function(){
+            this.clean();
+            this.draw();
         }
     }
 
     function draw(ctx, element) {
-        if (element !== null && element.hasOwnProperty("kind")) {
             switch (element.kind) {
                 case "rectangle":
                     ctx.fillRect(element.x, element.y, element.height, element.width);
                     break;
+                case "circle":
+                    ctx.beginPath();
+                    ctx.arc(element.x, element.y, element.radius, 0, 7);
+                    ctx.fill();
+                    ctx.closePath();
+                    break;
             }
-        }
     }
 }());
 
@@ -94,14 +125,18 @@
  */
 let board = new Board(900, 500);
 var bar = new Bar(30, 150, 200, 30, board);
-var bar = new Bar(830, 150, 200, 30, board);
+var bar_2 = new Bar(830, 150, 200, 30, board);
 let canvas = document.getElementById('canvas');
 let board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
+
+
 
 /**
  * read when up or down key is pressed
  */
 document.addEventListener("keydown", function (ev) {
+    ev.preventDefault();
     if (ev.keyCode == 38) {
         bar.up();
         console.log(ev.keyCode);
@@ -109,13 +144,22 @@ document.addEventListener("keydown", function (ev) {
     else if (ev.keyCode == 40) {
         bar.down();
         console.log(ev.keyCode);
+    } 
+    else if(ev.keyCode === 87){
+        bar_2.up();
+        console.log(ev.keyCode);
+    }
+    else if(ev.keyCode === 83){
+        bar_2.down();
+        console.log(ev.keyCode);
     }
 
 });
 
+window.requestAnimationFrame(main);
+/* self.addEventListener("load", main); */
 
-self.addEventListener("load", main);
 function main() {
-
-    board_view.draw();
+    board_view.play();
+    window.requestAnimationFrame(main);
 }
