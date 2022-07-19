bed2b9075d1c713c315ba41701f2aee194bbdea4
dcardonamoreno <danielcardona3d@gmail.com>
2022-07-19 03:00:19 -0500

    bars added

--- main.js
+++ main.js
@@ -28,6 +28,31 @@
 
 })();
 
+
+/**
+ * setear las barras
+ */
+(function(){
+    self.Bar = function(X,Y,width,height, board){
+        this.x = X;
+        this.y = Y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = 'rectangle';
+    }
+
+    /* self.Bar.prototype = {
+        down: function(){
+
+        },
+        up: function(){
+
+        }
+    } */
+}());
+
 /**
  * setear el board
  */
@@ -39,15 +64,47 @@
         this.board = board;
         this.ctx = canvas.getContext("2d");
     }
+
+    self.BoardView.prototype = {
+        draw: function () {
+            for (let i = this.board.elements.length - 1; i >= 0; i--) {
+                let el = this.board.elements[i];
+                draw(this.ctx, el);
+            }
+        }
+    }
+
+    function draw(ctx, element) {
+        if (element !== null && element.hasOwnProperty("kind")) {
+            switch (element.kind) {
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.height, element.width);
+                    break;
+            }
+        }
+    }
 }());
 
 /**
- * ejecutar main
+ * create objects bars and select canvas
  */
-window.addEventListener("load", main);
+let board = new Board(900, 500);
+var bar = new Bar(30, 150, 200, 30, board);
+var bar = new Bar(830, 150, 200, 30, board);
+let canvas = document.getElementById('canvas');
+let board_view = new BoardView(canvas, board);
+document.addEventListener("keydown", function (ev) {
+    if (ev.keyCode == 38) {
+        bar.up();
+    }
+    else if (ev.keyCode == 40) {
+        bar.down();
+    }
 
-function main(){
- var board = new Board(900, 500);
- var canvas = document.getElementById('canvas');
- var boardview = new BoardView(canvas, board);
+})
+
+self.addEventListener("load", main);
+function main() {
+
+    board_view.draw();
 }
